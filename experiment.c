77779433b28ed4273f9b15c71a5a83add973b118
Vernon Grant <vernon@ruppell.io>
2022-10-24 14:02:04 +0800

    Removes unused source

--- experiment.c
+++ experiment.c
@@ -11,22 +11,8 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    // Parse the JSON file.
-    jsn_handle root = jsn_from_file("./data/learn.json");
-    // Let's get an array from the root object other > array-of-numbers.
-    jsn_handle array = jsn_get(root, 2, "other", "array-of-numbers");
-    // Let's loop through each array item, we know that each array item is of
-    // type int.
-    for (unsigned int i = 0; i < jsn_array_count(array); i++) {
-        // Get the array item from the given index.
-        jsn_handle array_item = jsn_get_array_item(array, i);
-        // Print out the number.
-        printf("This array item's number is: %u \n",
-               jsn_get_value_int(array_item));
-    }
 
-    // Freeing
-    jsn_free(root);
+    printf("Hello World!\n");
 
     return EXIT_SUCCESS;
 }
